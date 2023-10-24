87455513be3b7964205361870e8591cd7d78566f
Richar <2958588231@qq.com>
2023-10-25 00:14:18 +0800

    添加一个接口

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
 import { ref } from "vue";
-const str = ref("hello, This is home page");
+import { useStore } from "../../store/index.ts";
+import {
+  HomeOutlined,
+  SettingFilled,
+  SmileOutlined,
+  SyncOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons-vue";
+const store = useStore();
+const str = ref("hello, This is login page");
+
+function add(): void {
+  store.increment();
+}
+
+function change(): void {
+  store.change();
+}
+
+function reset(): void {
+  store.$reset();
+}
 </script>
 <template>
   <div class="content">
-    {{ str }}
+    <a-space>
+      <home-outlined />
+      <setting-filled />
+      <smile-outlined />
+      <sync-outlined spin />
+      <smile-outlined :rotate="180" />
+      <loading-outlined />
+    </a-space>
+    {{ str }}计数{{ store.count }} name:{{ store.name }}
+    <a-button type="primary" @click="add">添加</a-button>
+    <a-button type="primary" @click="change">便便</a-button>
+    <a-button type="primary" @click="reset">重置</a-button>
   </div>
 </template>
 <style scoped></style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,42 +1,61 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { useStore } from "../../store/index.ts";
-import {
-  HomeOutlined,
-  SettingFilled,
-  SmileOutlined,
-  SyncOutlined,
-  LoadingOutlined,
-} from "@ant-design/icons-vue";
-const store = useStore();
-const str = ref("hello, This is login page");
-
-function add(): void {
-  store.increment();
+import { message } from "ant-design-vue";
+import axios from "axios";
+interface FormState {
+  username: string;
+  password: string;
 }
-
-function change(): void {
-  store.change();
-}
-
-function reset(): void {
-  store.$reset();
-}
+const formState = ref<FormState>({
+  username: "",
+  password: "",
+});
+const submit = () => {
+  console.log(formState.value.username);
+  axios
+    .post("http://127.0.0.1:4000/api/login", {
+      username: formState.value.username,
+      password: formState.value.password,
+    })
+    .then(function (res: any) {
+      console.log(res);
+      message.success(res.data.message);
+    })
+    .catch(function (err) {
+      console.log(err);
+      message.error(err.response.data.message);
+    });
+};
 </script>
 <template>
-  <div class="content">
-    <a-space>
-      <home-outlined />
-      <setting-filled />
-      <smile-outlined />
-      <sync-outlined spin />
-      <smile-outlined :rotate="180" />
-      <loading-outlined />
-    </a-space>
-    {{ str }}计数{{ store.count }} name:{{ store.name }}
-    <a-button type="primary" @click="add">添加</a-button>
-    <a-button type="primary" @click="change">便便</a-button>
-    <a-button type="primary" @click="reset">重置</a-button>
+  <div class="login">
+    <a-form
+      :model="formState"
+      name="basic"
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 16 }"
+      autocomplete="off"
+    >
+      <a-form-item
+        label="Username"
+        name="username"
+        :rules="[{ required: true, message: 'Please input your username!' }]"
+      >
+        <a-input v-model:value="formState.username" />
+      </a-form-item>
+
+      <a-form-item
+        label="Password"
+        name="password"
+        :rules="[{ required: true, message: 'Please input your password!' }]"
+      >
+        <a-input-password v-model:value="formState.password" />
+      </a-form-item>
+
+      <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+        <a-button type="primary" @click="submit">Submit</a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 <style scoped></style>
